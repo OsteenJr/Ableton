36a92a7aa2a88e6a8e9aa4e90dab7cf9e65ac935
OsteenJr <106804997+OsteenJr@users.noreply.github.com>
2024-12-19 21:07:12 +0100

    Add files via upload

---
+++ Ableton/script.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'; // Display the sidebar
+}
+
+// Hide sidebar
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'; // Hide the sidebar
+}
